ccdb617f0b1e769376f043cef6707831886ee697
Alex <alexeuel@gmail.com>
2020-08-12 08:39:09 +0300

    first commit

---
+++ lessonOne/js/script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+
+// console.log(number);
+// console.log(leftBorderWidth);
+
+// const obj = {
+//     a:10,
+// };
+
+// obj.a = 15;
+
+// console.log(obj.a);
+
+// console.log(name);
+
+// var name = 19;
+
+// {
+//     let i = 50;
+// }
+
+
+let number;
+
+console.log(number);
+
+if(number){
+    console.log('true');
+}
