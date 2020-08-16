68f3f08f87206b922539e64eb1c07fdd08208f0a
Alex <alexeuel@gmail.com>
2020-08-16 23:51:16 +0300

    modification for persone obgect

--- firstApp/js/main.js
+++ firstApp/js/main.js
@@ -1,27 +1,20 @@
 'use strict';
 
-let numberOfFilms;
 
 const persone = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
-
-
-function start(){
-    numberOfFilms = +prompt("How many films have you seen?", "");
-
-    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
-        numberOfFilms = +prompt("How many films have you seen?", "");
-    }
-}
-
-//start();
-
-    function rememberMyFilms(){
+    privat: false,
+    start: function () {
+        this.count = +prompt("How many films have you seen?", "");
+    
+        while(this.count == '' || this.count == null || isNaN(this.count)){
+            this.count = +prompt("How many films have you seen?", "");
+        }
+    },
+    rememberMyFilms: function(){
         while(true){
             let lastFilm = prompt("The lastest film what you were watching", ""),
             lastFilmsGrade = prompt("Write your grade of this film", "");
@@ -31,48 +24,52 @@ function start(){
             lastFilmsGrade = +lastFilmsGrade;
             if(lastFilm !== null && lastFilmsGrade !== null && lastFilm !== '' && lastFilmsGrade !== '' && 
             isNaN(lastFilmsGrade) === false && lastFilm.length < 50){
-                persone.movies[lastFilm] = lastFilmsGrade;
+                this.movies[lastFilm] = lastFilmsGrade;
             }
             else{
                 alert('input error, try again');
             }
         }
-    }
-
-   // rememberMyFilms();
-
-
-    function detectPersonalLevel(){
-        if(persone.count < 10){
+    },
+    detectPersonalLevel: function(){
+        if(this.count < 10){
             alert('not many');
         }
-        else if(persone.count >= 10 && persone.count <= 30){
+        else if(this.count >= 10 && this.count <= 30){
             alert('enought');
         }
-        else if(persone.count > 30){
+        else if(this.count > 30){
             alert('you are kinoman');
         }
         else{
             alert('error');
         }
-    }
-
-    //detectPersonalLevel();
-
-    function showMyDB(hidden){
-        if(!hidden){
-            console.log(persone);
+    },
+    showMyDB: function (){
+        if(!this.hidden){
+            console.log(this);
         }
-    }
-
-    showMyDB(persone.privat);
-
-    function writeYourGenres(p){
+    },
+    writeYourGenres: function(){
         for(let i = 1; i < 4; i++){
-            p.genres[i-1] = prompt(`your favorite genre number ${i}`);
+            let str = prompt(`your favorite genre number ${i}`);
+            if(str !== '' && str !== null ){
+                this.genres[i-1] = str;
+            }
+            else{
+                i--;
+            }
+        }
+        this.genres.forEach((element, number) => {
+            console.log(`my favorite ganre #${number + 1} - is ${element}`);
+        });
+    },
+    toggleVisiteMyDB: function(){
+        if(this.hidden){
+            this.hidden = false;
+        }
+        else{
+            this.hidden = true;
         }
     }
-
-    writeYourGenres(persone);
-
-
+};
--- objectsLesson/js/main.js
+++ objectsLesson/js/main.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const options = {
     name: 'test',
     width: 1024,
@@ -26,3 +26,33 @@ for( let key in options){
         console.log(`value ${key} = ${options[key]}`);   
     }
 }
+*/
+
+ const arr = [12, 28, 3, 4, 11];
+
+ console.log(arr.sort(compareNum));
+
+ function compareNum(a, b){
+     return a - b;
+ }
+
+// arr.pop(); //delite last el
+// arr.push(10); // add last el
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i} : ${item} in mass ${arr}`);
+// });
+
+
+// for(let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// for( let val of arr){
+//     console.log(val);
+// }
+
+// const str = prompt("Your products", "");
+// const products = str.split(', ');
+// console.log(products.sort());
+// console.log(products.join('; '));
