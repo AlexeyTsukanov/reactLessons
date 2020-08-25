69f6c32d5fadec482c961a9b8ad8fed8e22b0b64
Alex <alexeuel@gmail.com>
2020-08-25 15:04:58 +0300

    rewrite js in project

--- project/js/script.js
+++ project/js/script.js
@@ -1,98 +1,79 @@
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ],
-    arrOfBtns: [],
-    showMovie: function(){
-        movieDB.movies = movieDB.movies.map(v => v.toLowerCase());
-        movieDB.movies.sort();
-        let list = '';
-        for(let i = 0; i < movieDB.movies.length; i++){
-            list +=  
-            `<li class="promo__interactive-item">
-                ${i + 1} . ${movieDB.movies[i]} 
-                <div class="delete" data-movie-id="${i}"></div>
-            </li>`;
-        }
-        newMoviesSeenList.innerHTML = list;
-        
-    },
-    deliteMovie: function(id){
-        movieDB.movies = movieDB.movies.filter((i, n) => {
-            return n != id;
-        });
-        this.showMovie();
-        this.setBtnId();
-    },
-    setBtnId: () =>{
-        this.arrOfBtns = document.querySelectorAll('.delete');
-        this.arrOfBtns.forEach(i => {
-            i.addEventListener('click', () => {
-                movieDB.deliteMovie(i.dataset.movieId);
-            });
-        });
-    }
-};
+document.addEventListener('DOMContentLoaded', () => {
+
+    const spamBlock = document.querySelector('.promo__adv'),
+        spamImages = spamBlock.querySelectorAll('img'),
+        ganre = document.querySelector('.promo__genre'),
+        newBackground = document.querySelector('.promo__bg'),
+        moviesSeenList = document.querySelector('.promo__interactive-list'),
+        form = document.querySelector('.add'),
+        input = form.querySelector('.adding__input'),
+        checkbox = form.querySelector('[type=checkbox]');
 
 
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
+    const removeSpam = (arr) =>{
+        arr.forEach(i => i.remove());
+    };
 
-const spamBlock = document.querySelector('.promo__adv'),
-    spamImages = spamBlock.querySelectorAll('img'),
-    ganre = document.querySelector('.promo__genre'),
-    newBackground = document.querySelector('.promo__bg'),
-    moviesSeenList = document.querySelector('.promo__interactive-list'),
-    form = document.querySelector('.add'),
-    input = form.querySelector('.adding__input'),
-    sendBtn = form.querySelector('button'),
-    checkbox = form.querySelector('input[type=checkbox]');
+    const makeChanges = () => {
+        newBackground.style.cssText = 'background: url("img/bg.jpg"); background-size: cover;';
+        ganre.textContent = 'драма';
+    };
 
-
-    sendBtn.addEventListener('click', e => {
+    form.addEventListener('submit', e => {
         e.preventDefault();
+
         let str = input.value;
-        if(str.length > 21){
-            str = str.slice(0, 21) + '...';
+        if(str){
+            if(str.length > 21){
+                str = str.slice(0, 21) + '...';
+            }
             movieDB.movies.push(str);
+            if(checkbox.checked){
+                console.log('favorite film');
+            }
+            input.value = '';
+            showMovie(movieDB.movies, moviesSeenList);
         }
-        else{
-            movieDB.movies.push(str);
-        }
-        if(checkbox.checked){
-            console.log('favorite film');
-        }
-        input.value = '';
-        movieDB.showMovie();
-        movieDB.setBtnId();
     });
 
-    const newMoviesSeenList = document.createElement('div');
-    newMoviesSeenList.classList.add('promo__interactive-list');
+    const sortArr = (arr) => {
+        arr = arr.map(i => i.toLowerCase());
+        return arr.sort();
+    };
 
-    movieDB.showMovie();
+    const showMovie = (films, parrent) => {
+        films = sortArr(films);
+        parrent.innerHTML = "";
+        films.forEach((film, i) => {
+            parrent.innerHTML +=  
+            `<li class="promo__interactive-item">
+                ${i + 1} . ${film} 
+                <div class="delete"></div>
+            </li>`;
+        });
 
-    moviesSeenList.replaceWith(newMoviesSeenList);
+        document.querySelectorAll('.delete').forEach((btn , i) => {
+            btn.addEventListener('click', () => {
+                movieDB.movies.splice(i, 1);
+                showMovie(films, parrent);
+            });
+        });
+    };
 
-    movieDB.setBtnId();
+    removeSpam(spamImages);
+    makeChanges();
+    showMovie(movieDB.movies, moviesSeenList);
 
-    newBackground.style.cssText = 'background: url("img/bg.jpg"); background-size: cover;';
-    
-
-    ganre.textContent = 'драма';
-
-
-
-    spamImages.forEach(i => i.remove());
-
-
-
-
-    
-
-
+});
